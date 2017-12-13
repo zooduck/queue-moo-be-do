3beef712b4ed7cf5ded7c2f3e398ead057163f98
ZOODUCK <zooduck@beNiceOrEatSomeRice.com>
2017-12-13 17:43:19 +0000

    add new database structure with separate tables for staff and queues. add methods and functionality for creating and selecting queues.

--- admin_queue.js
+++ admin_queue.js
@@ -31,24 +31,45 @@ SERVER RESPONSIBILITIES:
 // ====================
 // API METHODS...
 // ====================
-const $http = (type, url) => {
+const $http = (type, url, formData=null) => {
 	const promise = new Promise(function(resolve, reject) {
 		const xhr = new XMLHttpRequest();
 		xhr.open(type, url);
 		// xhr.setRequestHeader('Accept', 'application/json');
-		xhr.send(null);
-		xhr.onload = (data) => {
+		xhr.send(formData);
+		xhr.onload = () => {
 			resolve(xhr.responseText);
 		}
 	});
-	return promise;	
+	return promise;
 }
 // creates a queue with the requested services
-const queueCreate = (services) => {
-	servicesCSV = services.join(",");
-	return $http("GET", `api.php?endpoint=QUEUE_CREATE&services=${servicesCSV}`);
+// const queueCreate = (services) => {
+// 	servicesCSV = services.join(",");
+// 	return $http("POST", `api.php?endpoint=QUEUE_CREATE&services=${servicesCSV}`);
+//
+// };
+const queueCreate = (formData) => {
+	//servicesCSV = services.join(",");
+	return $http("POST", `api.php?endpoint=QUEUE_CREATE`, formData);
 
 };
+
+const queuesGetAll = () => {
+	return $http("GET", "api.php?endpoint=QUEUES_GET_ALL");
+};
+
+const queueGetById = (id) => {
+	return $http("GET", `api.php?endpoint=QUEUE_GET_BY_ID&id=${id}`);
+};
+
+const staffGetByServices = (services) => {
+	return $http("GET", `api.php?endpoint=STAFF_GET_BY_SERVICES&services=${services}`)
+};
+
+
+
+
 // returns an object with an array of staff and customers
 const queueGet = () => {
 	return $http("GET", "api.php?endpoint=QUEUE_GET");
@@ -61,8 +82,8 @@ const queueDelete = () => {
 const customerAddToQueue = () => {
 
 };
-const customerDeleteFromQueue = (ticketRef) => {
-	return $http("DELETE", `api.php?endpoint=CUSTOMER_DELETE&ticketRef=${ticketRef}`)
+const customerDeleteFromQueue = (ticketRef, queueId) => {
+	return $http("DELETE", `api.php?endpoint=CUSTOMER_DELETE&ticketRef=${ticketRef}&queueId=${queueId}`);
 };
 const customerServe = () => {
 
@@ -112,26 +133,26 @@ const generateStaffTemplate = () => {
 };
 const generateCustomerTemplate = () => {
 	 let customerTemplate = "<div class=\"customer\" ticket-ref=\"%_TICKET_REF_%\">";
-	    customerTemplate += "<button class=\"customer--action-button customer--remove\">Remove</button>";	     
+	    customerTemplate += "<button class=\"customer--action-button customer--remove\">Remove</button>";
 	    customerTemplate += "<div class=\"customer__ticket-ref\">%_TICKET_REF_%</div>";
 	    customerTemplate += "<div class=\"customer__service\">Service: %_SERVICE_%</div>";
-    	customerTemplate += "<div class=\"customer__wait-time\">Estimated wait time: %_WAIT_TIME_%</div>";    
+    	customerTemplate += "<div class=\"customer__wait-time\">Estimated wait time: %_WAIT_TIME_%</div>";
     	customerTemplate += "</div>";
     return customerTemplate;
 };
 const addStaffMemberToDOM = (staffMember) => {
-	let staffTemplate = generateStaffTemplate();   
+	let staffTemplate = generateStaffTemplate();
     staffTemplate = staffTemplate.replace(/%_NAME_%/g, staffMember.name);
     staffTemplate = staffTemplate.replace(/%_STATUS_%/g, staffMember.status);
     staffTemplate = staffTemplate.replace(/%_SERVICES_%/g, staffMember.services);
-    staffTemplate = staffTemplate.replace(/%_SERVING_%/g, staffMember.serving);		
+    staffTemplate = staffTemplate.replace(/%_SERVING_%/g, staffMember.serving);
 	staffWrapper.innerHTML += staffTemplate;
 };
 const addCustomerToDOM = (customer) => {
-	let customerTemplate = generateCustomerTemplate();   
+	let customerTemplate = generateCustomerTemplate();
     customerTemplate = customerTemplate.replace(/%_TICKET_REF_%/g, customer.ticketRef);
     customerTemplate = customerTemplate.replace(/%_WAIT_TIME_%/g, customer.waitTime);
-    customerTemplate = customerTemplate.replace(/%_SERVICE_%/g, customer.service);		
+    customerTemplate = customerTemplate.replace(/%_SERVICE_%/g, customer.service);
 	customersWrapper.innerHTML += customerTemplate;
 };
 const clearView = () => {
@@ -155,12 +176,12 @@ const buildView = () => {
 	for (let staffMember of staff) {
 		let result = staffMember.services.find(function(service) {
 			return localQueueData.services.indexOf(service) != -1;
-		});		
-		if (result) {			
+		});
+		if (result) {
 			addStaffMemberToDOM(staffMember);
 		} else {
 			console.warn(`${staffMember.name} does not support any of the services for this queue!`);
-		}		
+		}
 	}
 	console.log("customers:", customers);
 	for (let customer of Array.from(customers)) {
@@ -171,20 +192,20 @@ const buildView = () => {
 	for (let button of removeCustomerButtons) {
 		button.addEventListener("click", function(e) {
 			let ticketRef = this.parentNode.getAttribute("ticket-ref");
-			customerRemove(ticketRef);
+			console.log(localQueueData);
+			customerRemove(ticketRef, localQueueData.id);
 		});
 	}
 };
-const customerRemove = (ticketRef) => {
-	customerDeleteFromQueue(ticketRef).then((queueData) => {
+const customerRemove = (ticketRef, queueId) => {
+	customerDeleteFromQueue(ticketRef, queueId).then((queueData) => {
 		console.log("queueData:", queueData);
 		if (queueDataChanged(JSON.parse(queueData), localQueueData)) {
 			// queue data is different! update view
 			console.warn("QUEUE DATA CHANGED!");
 			localQueueData = JSON.parse(queueData);
 			buildView();
-		}	
-		
+		}
 	});
 };
 
@@ -197,21 +218,92 @@ const resetDatabase = () => {
 };
 
 
+const createQueue = () => {
 
-queueGet().then((queueData) => {
-	console.log("queueGet() queueData:", queueData);
-	if (queueDataChanged(JSON.parse(queueData), localQueueData)) {
-		// queue data is different! update view
-		console.warn("QUEUE DATA CHANGED!");
-		localQueueData = JSON.parse(queueData);
-		buildView();
+	let formEl = document.querySelector("#queueCreate");
+	let formData = new FormData(formEl);
+	let services = [];
+	for (let pair of formData.entries()) {
+		let key = pair[0];
+		let val = pair[1];
+		if (val == "") {
+			alert("QUEUE_NAME_CANNOT_BE_BLANK");
+			return;
+		}
+		if (key != "id") {
+			services.push(key);
+		}
+		console.log(key, val);
 	}
+	formData.append("services", services);
+
+	console.log("services:", services);
+
+	 queueCreate(formData).then((queueData) => {
+		 console.log("QUEUE CREATED, QUEUES ARRAY UPDATED!", JSON.parse(queueData));
+	 });
+};
+
+
+const getQueueById = () => {
+	let id = document.querySelector("#queueSelect").value;
+	let queue = {};
+	queueGetById(id).then((queueData) => {
+		console.log("GET QUEUE BY ID", JSON.parse(queueData));
+
+		let services = JSON.parse(queueData).services;
+		queue.id = JSON.parse(queueData).id;
+		queue.customers = JSON.parse(queueData).customers;
+		queue.services = services;
+
+		console.log("services for selected queue", services);
+
+		staffGetByServices(services).then((staffData) => {
+			queue.staff = JSON.parse(staffData);
+			console.log("staff get by services", queueData);
+
+			console.log("NEW QUEUE OBJECT!", queue);
+
+			localQueueData = queue;
+			buildView();
+
+		});
+
+
+	});
+};
+
+// queueGet().then((queueData) => {
+// 	console.log("queueGet() queueData:", queueData);
+// 	if (queueDataChanged(JSON.parse(queueData), localQueueData)) {
+// 		// queue data is different! update view
+// 		console.warn("QUEUE DATA CHANGED!");
+// 		localQueueData = JSON.parse(queueData);
+// 		buildView();
+// 	}
+// });
+
+
+
+// get all queues and add them to dropdown list...
+queuesGetAll().then((queueData) => {
+	let allQueues = JSON.parse(queueData).queues;
+	const queueSelectEl = document.querySelector("#queueSelect");
+	queueSelectEl.innerHTML = "";
+	for (let queue of allQueues) {
+		console.log("queue id", queue.id);
+		let opt = document.createElement("OPTION");
+		opt.innerHTML = queue.id;
+		queueSelectEl.appendChild(opt);
+	}
+	console.log("ALL QUEUES:", JSON.parse(queueData));
 });
-queueCreate(['tire_fit', 'mot']).then((queueData) => {
-	console.log(queueData);	
-});
+
+// queueCreate(['tire_fit', 'mot']).then((queueData) => {
+// 	console.log(queueData);
+// });
 queueDelete().then((queueData) => {
-	console.log(queueData);	
+	console.log(queueData);
 });
 
 // customerDeleteFromQueue("13").then((queueData) => {
@@ -227,6 +319,6 @@ queueDelete().then((queueData) => {
 // 	console.log("queueData changed?:", queueDataChanged(queue_data, queueData));
 // 	//console.log(JSON.stringify(queue_data) == JSON.stringify(queueData));
 // 	console.log("queueData", queueData);
-// 	console.log("queue_data", queue_data);	
+// 	console.log("queue_data", queue_data);
 // 	});
 // }, 1000);
--- api.php
+++ api.php
@@ -16,7 +16,51 @@ if ($endpoint == "QUEUE_RESET") {
 	echo fread($updatedDb, filesize("db.json"));
 }
 
+// =====================
+// QUEUES_GET_ALL
+// =====================
+if ($endpoint == "QUEUES_GET_ALL") {
+	$jsonDb = fopen("table_queues.db.json", "r");
+	echo fread($jsonDb, filesize("table_queues.db.json"));
+}
 
+
+// =====================
+// QUEUE_GET_BY_ID
+// =====================
+if ($endpoint == "QUEUE_GET_BY_ID") {
+	$id = $_GET["id"];
+	$tableQueues = file_get_contents("table_queues.db.json");
+	$tableQueuesObj = json_decode($tableQueues, true);
+	$queue = new class{};
+	foreach($tableQueuesObj["queues"] as $key => $val) {
+		// echo "key" . $key . "val" .  $val;
+		if ($val["id"] == $id) {
+			$queue = $val;
+		}
+	}
+	echo json_encode($queue);
+}
+// ========================
+// STAFF_GET_BY_SERVICES
+// ========================
+if ($endpoint == "STAFF_GET_BY_SERVICES") {
+	$services = explode(",", $_GET["services"]);
+	$staffWithServices = [];
+	$tableStaff = file_get_contents("table_staff.db.json");
+	$tableStaffObj = json_decode($tableStaff, true);
+	foreach($tableStaffObj["staff"] as $staffMember) {
+		foreach($services as $service) {
+			if (in_array($service, $staffMember["services"])) {
+				array_push($staffWithServices, $staffMember);
+				break;
+			}
+		}
+	}
+
+	echo json_encode($staffWithServices);
+
+}
 
 // =====================
 // QUEUE_GET
@@ -30,7 +74,30 @@ if ($endpoint == "QUEUE_GET") {
 // QUEUE_CREATE
 // =====================
 if ($endpoint == "QUEUE_CREATE") {
-	echo "QUEUE_CREATE..." . $_GET["services"];
+	$id = $_POST["id"];
+	$services = $_POST["services"];
+
+	$queueObj = []; //new class{};
+	$queueObj["id"] = $id;
+	$queueObj["services"] = explode(",", $services);
+	$queueObj["customers"] = [];
+
+
+	$tableQueues = file_get_contents("table_queues.db.json");
+	$tableQueuesObj = json_decode($tableQueues, true); // json -> object
+  array_push($tableQueuesObj["queues"], $queueObj);
+
+	$tableQueuesJSON = json_encode($tableQueuesObj);
+
+
+	$db = fopen("table_queues.db.json", "w");
+	fwrite($db, $tableQueuesJSON);
+
+	$updatedDb = fopen("table_queues.db.json", "r");
+
+	// ECHO DATABASE AS A JSON...
+	echo fread($updatedDb, filesize("table_queues.db.json"));
+
 }
 // =====================
 // QUEUE_DELETE
@@ -45,22 +112,63 @@ if ($endpoint == "QUEUE_DELETE") {
 // ============================
 if ($endpoint == "CUSTOMER_DELETE") {
 
-	$jsonDb = file_get_contents("db.json");
+	//$file = "db.json";
+	$file = "table_queues.db.json";
+
+	$jsonDb = file_get_contents($file);
 	$jsonDbObj = json_decode($jsonDb, true);
 
 	$ticketRef = $_GET["ticketRef"];
-	
-	function filterByTicketRef($customer) {
-		return $customer["ticketRef"] != $_GET["ticketRef"];
-	}	
-	$filteredCustomers = array_filter($jsonDbObj["customers"], "filterByTicketRef");
+	$queueId = $_GET["queueId"];
 
-	
+
+	$queues = $jsonDbObj["queues"];
+
+	function filterByTicketRef($customer) {
+		echo "is " . $customer["ticketRef"] . "!=" . $_GET["ticketRef"];
+		return $customer["ticketRef"] != $_GET["ticketRef"];
+	}
+
+	foreach($queues as $queue) {
+		echo "is " . $queueId . "==" . $queue["id"];
+		if ($queue["id"] == $queueId) {
+			echo "QUEUE IS SAME>>>>>>>>>>>>>>>>>>>>>>>>>";
+			$filteredCustomers = array_filter($queue["customers"], "filterByTicketRef");
+		}
+	}
+
+	//echo "filteredCustomers" . json_encode($filteredCustomers);
+
+	foreach($jsonDbObj["queues"] as $queue) {
+		if ($queue["id"] == $queueId) {
+			echo "GOT HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE";
+			// THIS DOES NOT SEEM TO ALTER THE jsonDbObj!!!! WHYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY NOT?????????????????????????????
+			$queue["customers"] = $filteredCustomers;
+		}
+	}
+	echo "you asked me to remove " . $ticketRef . " from queue id of " . $queueId;
+	echo "NEW QUEUE DATABASE LOOKS LIKE =>" . json_encode($jsonDbObj);
+	exit;
+
+
+
+
+
+
+
+
+
+	function filterByTicketRef_ERR($customer) {
+		return $customer["ticketRef"] != $_GET["ticketRef"];
+	}
+	$filteredCustomers = array_filter($jsonDbObj["customers"], "filterByTicketRef_ERR");
+
+
 	$jsonDbObj["customers"] = [];
 	foreach ($filteredCustomers as $customer) {
 		array_push($jsonDbObj["customers"], $customer);
 	}
-	
+
 	$updatedJsonDb = json_encode($jsonDbObj);
 
 	$db = fopen("db.json", "w");
